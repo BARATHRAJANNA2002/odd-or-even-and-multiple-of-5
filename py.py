beec59593cdc1bce3c720b2f4e450e2441514831
BARATHRAJANNA2002 <barathchitra2002@gmail.com>
2022-10-13 08:16:41 -0700

    first commit~

---
+++ py.py
@@ -0,0 +1,19 @@
+
+##add or even
+
+##num=int(input("enter the number:"))
+##if(num%2)==0:
+##    print("{0}is even".format(num))
+##else:
+##    print("{0}is odd".format(num))
+
+
+
+##multiple of 5
+
+##num=int(input("enter the number:"))
+##
+##if(num%5==0):
+##    print("{0}is multiple of 5".format(num))
+##else:
+##    print("{0}is not multiple of 5".format(num))
